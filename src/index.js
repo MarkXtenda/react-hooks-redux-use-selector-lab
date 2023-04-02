494d718fa03d919d36d66fde72e7fc178b5d2d47
MarkXtenda <mark.i2p.1998@gmail.com>
2023-04-02 15:14:00 +1000

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { legacy_createStore } from "redux";
 import App from "./App";
-
 import usersReducer from "./features/users/usersSlice";
+import { Provider } from "react-redux";
 
 // add imports and code
+const store = legacy_createStore(
+  usersReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   // add imports and code
-  <App />,
+  <Provider store = {store}>
+  <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
